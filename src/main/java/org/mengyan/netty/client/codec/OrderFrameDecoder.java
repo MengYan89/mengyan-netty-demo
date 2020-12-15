0aa446925296146c63bc6e799f9a7488752c9cf1
mengyan <my327827856@163.com>
2020-12-15 17:10:01 +0800

    为LengthFieldBasedFrameDecoder添加注意注释

--- src/main/java/org/mengyan/netty/client/codec/OrderFrameDecoder.java
+++ src/main/java/org/mengyan/netty/client/codec/OrderFrameDecoder.java
@@ -2,6 +2,11 @@ package org.mengyan.netty.client.codec;
 
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 
+/**
+ *
+ * 注意：LengthFieldBasedFrameDecoder如果不需要解析出Length头只需要内容的话，请设置initialBytesToStrip变量
+ *      设置的长度应与lengthFieldLength相等
+ */
 public class OrderFrameDecoder extends LengthFieldBasedFrameDecoder {
     public OrderFrameDecoder() {
         super(Integer.MAX_VALUE, 0, 2, 0, 2);
